c1d0b62f961b19d8b8109dcb2606a6144529830b
dev <dev@Patricks-MacBook-Pro.local>
2021-07-14 03:31:53 +0300

    Added my project

--- src/components/ThemeUI/Metamask.js
+++ src/components/ThemeUI/Metamask.js
@@ -27,13 +27,7 @@ function Metamask() {
 }
 
 export default () => (
-  <UseWalletProvider
-    chainId={1}
-    connectors={{
-      // This is how connectors get configured
-      portis: { dAppId: "my-dapp-id-123-xyz" },
-    }}
-  >
+  <UseWalletProvider chainId={1}>
     <Metamask />
   </UseWalletProvider>
 );
--- src/components/ThemeUI/MetamaskTwo.js
+++ src/components/ThemeUI/MetamaskTwo.js
@@ -27,13 +27,7 @@ function MetamaskTwo() {
 }
 
 export default () => (
-  <UseWalletProvider
-    chainId={1}
-    connectors={{
-      // This is how connectors get configured
-      portis: { dAppId: "my-dapp-id-123-xyz" },
-    }}
-  >
+  <UseWalletProvider chainId={1}>
     <MetamaskTwo />
   </UseWalletProvider>
 );
